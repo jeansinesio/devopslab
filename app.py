233d28380a75771202cefca851a08e0f71fbc78b
jeansinesio <jeanclaytonsinesio@gmail.com>
2022-01-28 00:35:45 -0300

    Fix UnitTest

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - JeanSinesio"
+    return "Hello World - Jean Sinesio"
 
 if __name__ == '__main__':
     app.run()
